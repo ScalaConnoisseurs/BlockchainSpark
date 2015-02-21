940dade9ef9ed73608c8f944fc33daa6ffcffe4e
Joseph Goulden <joegoulden@gmail.com>
2015-02-21 15:15:01 +0000

    Add parsing for output addresses

--- src/main/scala/model/bitcoin/UnconfirmedTransaction.scala
+++ src/main/scala/model/bitcoin/UnconfirmedTransaction.scala
@@ -2,11 +2,13 @@ package model.bitcoin
 
 import spray.json._
 
-case class UnconfirmedTransaction(lockTime: Int, ver: Int, size: Int, inputs: Array[Input], time: Long, tx_index: Long, vin_sz: Int, hash: String)
+case class UnconfirmedTransaction(lockTime: Int, ver: Int, size: Int, inputs: Array[Input], time: Long, tx_index: Long, vin_sz: Int, hash: String, outputs: Array[Outputs])
 
 case class Input(sequence: Long, previousOut: PreviousOut)
 
 case class PreviousOut(address: String)
+
+case class Outputs(address: String)
 
 object PreviousOutJsonProtocol extends DefaultJsonProtocol {
   implicit object PreviousOutTransactionFormat extends  RootJsonFormat[PreviousOut] {
@@ -27,18 +29,29 @@ object InputJsonProtocol extends DefaultJsonProtocol {
       case Seq(JsNumber(sequence), prevOut) => new Input(sequence.toLong, prevOut.convertTo[PreviousOut])
     }
   }
-  
+}
+
+object OutputsJsonProtocol extends DefaultJsonProtocol {
+  implicit object OutputsTransactionFormat extends RootJsonFormat[Outputs] {
+    override def read(json: JsValue): Outputs = json.asJsObject.getFields("addr") match {
+      case Seq(JsString(address)) => new Outputs(address)
+    }
+
+    override def write(obj: Outputs): JsValue = ???
+  }
 }
 
 object TransactionJsonProtocol extends DefaultJsonProtocol {
   import InputJsonProtocol._
+  import OutputsJsonProtocol._
+  
   implicit object UnconfirmedTransactionFormat extends RootJsonFormat[UnconfirmedTransaction] {
     def write(uct: UnconfirmedTransaction) = ???
 
-    def read(value: JsValue) = value.asJsObject.getFields("size", "tx_index", "lock_time", "out", "vout_sz", "hash", "inputs", "ver", "relayed_by", "time", "vin_sz") match {
-      case Seq(JsNumber(size), JsNumber(tx_index), JsNumber(lock_time), JsArray(out), JsNumber(vout_sz), JsString(hash), JsArray(inputs), JsNumber(ver), JsString(relayed_by),
+    def read(value: JsValue) = value.asJsObject.getFields("size", "tx_index", "lock_time", "out", "vout_sz", "hash", "inputs", "ver", "relayed_by", "time", "vin_sz", "out") match {
+      case Seq(JsNumber(size), JsNumber(tx_index), JsNumber(lock_time), JsArray(outputs), JsNumber(vout_sz), JsString(hash), JsArray(inputs), JsNumber(ver), JsString(relayed_by),
     JsNumber(time), JsNumber(vin_sz)) =>
-        new UnconfirmedTransaction(lock_time.toInt, ver.toInt, size.toInt, inputs.map(_.convertTo[Input]).toArray, time.toLong, tx_index.toLong, vin_sz.toInt, hash)
+        new UnconfirmedTransaction(lock_time.toInt, ver.toInt, size.toInt, inputs.map(_.convertTo[Input]).toArray, time.toLong, tx_index.toLong, vin_sz.toInt, hash, outputs.map(_.convertTo[Outputs]).toArray)
       case _ => deserializationError("Unconfirmed transaction expected")
     }
   }
